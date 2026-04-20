c50f02e4929067a4aaa31908335d8c02490c764d
Luanhotlinebr <luanhotlinebr60@gmail.com>
2026-04-20 11:54:27 -0300

    feat:criando pagina home

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container"
+
+export default function Dashboard(){
+  return (
+  <Container>
+    <h1>Página dashboard</h1>
+  </Container>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Home() {
   return (
-   <><h1>Teste</h1></>
+   <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+   <h1 className="font-medium text-2xl mb-2 ">Gerencia sua empresa</h1>
+   <h2 className="font-bold text-3xl mb-8 text-blue-500 md:text-4xl">Atendimentos, clientes</h2>
+   </main>
   );
 }
