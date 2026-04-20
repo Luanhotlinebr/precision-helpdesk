c88ad73c7c5ee9430901d55da1a848ae1f53bf57
Luanhotlinebr <luanhotlinebr60@gmail.com>
2026-04-20 14:03:31 -0300

    feat:configurando next-auth e suas dependencias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/providers/auth";
 
 //Componentes globais
 import { Header } from "@/components/header";
@@ -31,8 +32,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <AuthProvider>
         <Header/>
-        {children}</body>
+        {children}
+        </AuthProvider>
+</body>
     </html>
   );
 }
