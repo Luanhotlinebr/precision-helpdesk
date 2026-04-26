1509e93eac05c30be43a1b7949f21c05ae0dc5b6
Luanhotlinebr <luanhotlinebr60@gmail.com>
2026-04-25 22:24:37 -0300

    style:diminuindo cor de bg do botão de deletar

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -2,7 +2,7 @@ import { Mail } from "@deemlol/next-icons"
 import { Phone } from "@deemlol/next-icons"
 export function CardCustomer(){
   return(
-    <article className="flex flex-col border-2 bg-gray-100 p-3 rounded-lg hover:scale-102 duration-200 gap-2">
+    <article className="flex flex-col border-2 bg-gray-100 p-3 rounded-lg hover:scale-101 duration-200 gap-2">
       <h2>
         <a className="font-bold">Nome:</a> Bruna Rafaela
       </h2>
@@ -12,7 +12,7 @@ export function CardCustomer(){
       <div className="flex items-center gap-2">
          <a className="font-bold"><Phone/></a> <p>86 99999-9999</p>
       </div>
-      <button className="self-start text-white hover:text-black bg-red-400 rounded px-4 py-2 cursor-pointer hover:bg-red-700">
+      <button className="self-start text-white  bg-red-400 rounded px-4 py-2 cursor-pointer hover:bg-red-500">
         Deletar
       </button>
     </article>
